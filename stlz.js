5abfe3fe7129e8909faa1a9a5bc7f6bea65bf9f0
inam ul haq <inamhaq1997@gmail.com>
2021-08-30 11:22:44 +0530

    first copy

---
+++ stlz.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $('.slider').slick({
+         
+        dots: true,
+        Infinity: false,
+        arrows: true,
+          
+          
+        appendDots:'.slider-dots',
+        dotsClass:'dots'
+    }
+    
+     
+     
+    
+    );
+    
+});
+
+
+
+
+ 
